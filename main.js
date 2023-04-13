80334cb060bb9c04f5d4235625e7e03dfb065a30
Shuvam212312 <130625526+Shuvam212312@users.noreply.github.com>
2023-04-12 19:57:41 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function add(){
+    var result=Number(document.getElementById("fn").value)+Number(document.getElementById("sn").value);
+    document.getElementById("b1").value=result;
+ 
+   
+}
+
+function subtract(){
+    var result=Number(document.getElementById("fn").value)-Number(document.getElementById("sn").value);
+    document.getElementById("b2").value=result;
+ 
+   
+}
+
+
+function divison(){
+    var result=Number(document.getElementById("fn").value)/Number(document.getElementById("sn").value);
+    document.getElementById("b4").value=result;
+ 
+   
+}
+
+function multiply(){
+    var result=Number(document.getElementById("fn").value)*Number(document.getElementById("sn").value);
+    document.getElementById("b3").value=result;
+ 
+   
+}
